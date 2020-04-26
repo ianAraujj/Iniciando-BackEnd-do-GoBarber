a8d6715600f027180cebc6a41fb50e33d595bbf9
Ian Luccas <ianlucas2503@gmail.com>
2020-04-26 18:34:44 -0300

    Foi feita a conexão com o Banco de Dados Postgre

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import Router from 'express';
+import User from './app/models/User'
 
 const routes = new Router();
 
 
-routes.get('/', (req, res) =>{
-  return res.json({
-    MESSAGE: "HELLO WORLD !!"
-  });
+routes.get('/', async (req, res) =>{
+  const user = await User.create({
+    name: 'Ian Luccas',
+    email: 'ian@gmail.com',
+    password_hash: '148716498',
+    provider: true
+  })
+
+  return res.json(user);
 })
 
 export default routes;
