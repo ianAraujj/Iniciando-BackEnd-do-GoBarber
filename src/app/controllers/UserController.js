c5db3ebec02e9d5b01e8b85b9421ae564097b68d
Ian Luccas <ianlucas2503@gmail.com>
2020-04-27 07:38:57 -0300

    Criando o primeiro Controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,73 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res){
+
+    if((req.body.name) && (req.body.email) && (req.body.password_hash)){
+      const email_exist = await User.findOne({where:{ email: req.body.email}});
+
+      if(email_exist){
+        return res.status(400).json({
+          ERROR: "e-mail already exists"
+        });
+      }
+  
+      const {id, name, email, provider} = await User.create(req.body);
+  
+      return res.json({
+        id: id,
+        name: name,
+        email: email,
+        provider: provider
+      });
+    }else{
+      return res.status(400).json({
+        ERROR: "Campos incorretos"
+      });
+    } 
+  }
+
+  async delete(req, res){
+    const id_recebido = req.params.id;
+    const user = await User.findOne({where:{
+      id: id_recebido
+    }});
+
+    if(user){
+
+      //await User.delete(user);
+      return res.status(400).json({
+        MESSAGE: "User was deleted"
+      })
+
+    }else{
+      return res.status(400).json({
+        ERROR: "User not exists"
+      })
+    }
+  }
+
+  async show(req, res){
+    const id_recebido = req.params.id;
+    const user = await User.findOne({where:{
+      id: id_recebido
+    }});
+
+    if(user){
+      return res.json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        provider: user.provider
+      });
+    }else{
+      return res.status(400).json({
+        ERROR: "User not found"
+      })
+    }
+  }
+
+}
+
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,11 @@
 import Router from 'express';
-import User from './app/models/User'
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 
-routes.get('/', async (req, res) =>{
-  const user = await User.create({
-    name: 'Ian Luccas',
-    email: 'ian@gmail.com',
-    password_hash: '148716498',
-    provider: true
-  })
-
-  return res.json(user);
-})
+routes.post('/users', UserController.store);
+routes.delete('/users/:id', UserController.delete);
+routes.get('/users/:id', UserController.show);
 
 export default routes;
