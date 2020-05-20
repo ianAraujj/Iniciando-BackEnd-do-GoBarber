8c1b4e082baa0d805e00d5f7f1237e0d44e5b9e6
Ian Luccas <ianlucas2503@gmail.com>
2020-05-20 10:36:16 -0300

    Fazendo upload de imagem

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Router from 'express';
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionsController';
 import Auth from './app/middlewares/auth';
+import multerConfig from './config/multer';
+import multer from 'multer';
 
 const routes = new Router();
 
@@ -12,9 +14,17 @@ routes.post('/sessions', SessionsController.store);
 // - Agora eu vou adicionar um Middleware Global. Ele será válido para todas as 
 // rotas ABAIXO dele
 
+const upload = multer(multerConfig);
+
 routes.use(Auth);
 routes.put('/users', UserController.update);
-// routes.delete('/users', UserController.delete);
 routes.get('/users', UserController.show);
+
+// 'file' vai ser o nome do campo
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({
+    ok: "True"
+  })
+})
 
 export default routes;
